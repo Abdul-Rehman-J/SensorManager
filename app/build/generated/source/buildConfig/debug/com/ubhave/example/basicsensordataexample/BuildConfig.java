6750d0875649c1732f2e4f022d15d420862ebf1a
Abdul Rehman <ashirjaved67@gmail.com>
2017-03-15 14:39:54 +0500

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/ubhave/example/basicsensordataexample/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.ubhave.example.basicsensordataexample;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.ubhave.example.basicsensordataexample";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
